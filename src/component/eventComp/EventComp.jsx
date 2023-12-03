aff9484b0ef3eff56b52dc7e8c6c246441db55d1
nelaanm <-->
2023-12-03 15:46:48 +0700

    update event highlight

--- src/component/eventComp/EventComp.jsx
+++ src/component/eventComp/EventComp.jsx
@@ -1,11 +1,20 @@
-import React, { useEffect, useState } from "react";
-import Slider from "react-slick";
+import React, { useCallback, useEffect, useState } from "react";
+import headerLogo from "../../assets/img/Logo.png";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 import axios from "axios";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 function EventComp({ posts }) {
   const [data, setData] = useState([]);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,33 +32,6 @@ function EventComp({ posts }) {
     fetchData();
   }, []);
 
-  // Configure settings for the slider
-  const sliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <div className="flex bg-color7">
       <div className="mx-auto mt-10 px-10 md:px-15 lg:px-32">
@@ -60,33 +42,53 @@ function EventComp({ posts }) {
           Perubahan Dimulai di Sini: Event Anti-Bullying Community
         </h2>
         <div className="mt-10 mb-10">
-          {" "}
-          <Slider {...sliderSettings}>
-            {posts.map((item) => (
-              <div key={item.id} className="m-4 cursor-pointer">
-                <div className="bg-white border border-gray-300 rounded-lg overflow-hidden">
-                  <img
-                    src={item.image}
-                    alt={item.title}
-                    className="w-full object-cover aspect-w-1 aspect-h-1 rounded-tl-lg rounded-tr-lg"
-                  />
-                  <h3 className="font-jakarta font-bold mt-3 mb-2 mx-2">
-                    {item.title}
-                  </h3>
+          <div className="embla overflow-hidden" ref={emblaRef}>
+            <div className="embla__container flex">
+              {posts.map((item) => (
+                <div
+                  key={item.id}
+                  className="embla__slide flex-[0_0_100%] md:flex-[0_0_50%] hover:shadow-md w-full md:min-w-lg max-w-xl border border-gray-300  bg-white rounded-lg m-4 cursor-pointer"
+                >
+                  <div className="embla__slide__inner flex">
+                    <img
+                      src={item.image}
+                      alt={item.title}
+                      className="object-cover h-full md:h-[200px] rounded-s-lg"
+                    />
+                    <h3 className="font-jakarta font-bold mt-3 mb-2 mx-2">
+                      {item.title}
+                    </h3>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </Slider>
+              ))}
+            </div>
+            <div className="flex justify-center space-x-2">
+              <button
+                className="embla__prev font-bold text-white bg-color5 shadow-md py-1 px-3 rounded-lg"
+                onClick={scrollPrev}
+              >
+                Prev
+              </button>
+              <button
+                className="embla__next font-bold text-white bg-color5 shadow-md py-1 px-3 rounded-lg"
+                onClick={scrollNext}
+              >
+                Next
+              </button>
+            </div>
+          </div>
         </div>
         <div className="flex justify-center items-center flex-col bg-color3 rounded-lg mt-10 mb-10">
           <img
-            src="" // Replace 'logo.png' with your actual logo file path
-            alt="Logo"
-            className="w-20 h-20 mt-8" // Adjust width and height as needed
+            src={headerLogo}
+            className="w-12 hover:scale-110 transition-all duration-100 my-5"
+            alt="Safe Space Squad"
           />
           <div className="text-center mb-8">
             <h1 className="font-jakarta font-bold text-xl text-black mx-4">
-            "Ketika kita menyatukan kekuatan kita melawan perundungan, kita menciptakan dunia yang lebih aman dan lebih baik untuk semua orang."
+              "Ketika kita menyatukan kekuatan kita melawan perundungan, kita
+              menciptakan dunia yang lebih aman dan lebih baik untuk semua
+              orang."
             </h1>
           </div>
         </div>
